f1355907c3ffe9029b8a44dac3a94265ed1a8d7b
Chiamaka Ikenna-Uzodike <chiamakauzodike@gmail.com>
2025-01-10 13:27:26 +0000

    adding the task managemnet project

--- task_management_api/task_management_api/settings.py
+++ task_management_api/task_management_api/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DB_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # adding the heroku domain for production
 ALLOWED_HOSTS = ['chiamakauzodike.pythonanywhere.com']
